046b87ff08e12cad64d2ea98c4bc3b667d414d75
Damodaran R <damodaran_r@outlook.com>
2018-03-29 19:21:33 +0530

    helloworld

--- helloworld.js
+++ helloworld.js
@@ -6,4 +6,4 @@ res.writeHead(200, {'content-type':'text/plain'});
 res.end('Hello World!');
 }).listen(8001);
 
-console.log('Application is running on the port over 8001 yes !!'!);
+console.log('Application is running on the port over 8001 yes !!');
